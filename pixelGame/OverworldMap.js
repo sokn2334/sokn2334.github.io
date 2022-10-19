9ef2a4941f637ce36688e848c9694dd43043d555
sokn2334 <sokn2334@colorado.edu>
2022-10-18 20:51:19 -0600

    GameStart

---
+++ pixelGame/OverworldMap.js
@@ -0,0 +1,59 @@
+class OverworldMap{
+    constructor(config){
+        this.gameObjects = config.gameObjects;
+
+        this.lowerImage = new Image();
+        this.lowerImage.src = config.lowerSrc;
+        
+        this.upperImage = new Image();
+        this.upperImage.src = config.upperSrc;
+    }
+
+    drawLowerImage(ctx){
+        ctx.drawImage(this.lowerImage,0,0)
+    }
+    drawUpperImage(ctx){
+        ctx.drawImage(this.upperImage,0,0)
+    }
+}
+
+
+window.OverworldMaps ={
+    DemoRoom:{
+        lowerSrc: "images/maps/DemoLower.png",
+        upperSrc: "images/maps/DemoUpper.png",
+        gameObjects:{
+            cat: new Person({
+                isPlayerControlled: true,
+                x:utils.widthGrid(5),
+                y:utils.widthGrid(6),
+            }),
+            npc1: new Person({
+                x:utils.widthGrid(7),
+                y:utils.widthGrid(9),
+                src: "images/characters/npc1.png",
+            }),
+        }
+    },
+
+    Kitchen:{
+        lowerSrc: "images/maps/KitchenLower.png",
+        upperSrc: "images/maps/KitchenUpper.png",
+        gameObjects:{
+            cat: new GameObject({
+                x:3,
+                y:5,
+            }),
+            npcA: new GameObject({
+                x:9,
+                y:6,
+                src: "images/characters/npc2.png",
+            }),
+            npcB: new GameObject({
+                x:10,
+                y:8,
+                src: "images/characters/npc3.png",
+            }),
+        }
+    }
+}
